90a486d3afeff6a27f27bc0f5db94415e8bd945c
Bruno Sajermann <contato@sajermann.com>
2022-08-16 11:08:49 -0300

    adjusts

--- src/ComponentsInternal/CodeBlock/index.tsx
+++ src/ComponentsInternal/CodeBlock/index.tsx
@@ -1,8 +1,40 @@
+import { Fragment } from 'react';
+import { OptionButton } from '../../components/OptionButton';
 import styles from './styles.module.css';
 
 type Props = {
-	children: JSX.Element;
+	children: React.ReactNode;
 };
+
+export function JsCode({ children }: Props) {
+	return <Fragment key={41231}>{children}</Fragment>;
+}
+
+export function TsCode({ children }: Props) {
+	return children;
+}
+
 export function CodeBlock({ children }: Props) {
-	return <code>{children}</code>;
+	const t = children as {
+		type: {
+			name: string;
+		};
+	};
+	console.log({ children });
+	console.log({ t });
+	// console.log(t?.type.name);
+	// console.log(t?.type.name === 'JsCode' || '');
+	return (
+		<div>
+			<code>
+				{children}
+				<OptionButton className={styles.choiseLanguage}>
+					Javascript
+				</OptionButton>
+				<OptionButton className={styles.choiseLanguage}>
+					Typescript
+				</OptionButton>
+			</code>
+		</div>
+	);
 }
--- src/Pages/Docs/Getting Started/index.tsx
+++ src/Pages/Docs/Getting Started/index.tsx
@@ -1,3 +1,4 @@
+import { CodeBlock, JsCode } from '../../../ComponentsInternal/CodeBlock';
 import { useDarkMode } from '../../../Hooks/DarkMode';
 import styles from './styles.module.css';
 
@@ -5,14 +6,55 @@ export default function GettingStarted() {
 	const { darkMode } = useDarkMode();
 	return (
 		<div>
-			<h1>Introdução</h1>
-			Sajermann UI é um conjunto de componentes para React de interface de
-			usuário criado para facilitar o uso de determinados componentes que na
-			maior parte das vezes se repetiam em todos os projetos.
-			<h1>Instalação</h1>
-			Sajermann UI está disponível como um pacote NPM e para instalar em seu
-			projeto basta executar:
-			<code>npm install @sajermann/ui-react</code>
+			<div className={styles.block}>
+				<h1>Introdução</h1>
+				<span>
+					Sajermann UI é um conjunto de componentes para React de interface de
+					usuário criado para facilitar o uso de determinados componentes que na
+					maior parte das vezes se repetiam em todos os projetos.
+				</span>
+			</div>
+			<div className={styles.block}>
+				<h1>Instalação</h1>
+				<span>
+					Sajermann UI está disponível como um pacote NPM e para instalar em seu
+					projeto basta executar:
+				</span>
+				<CodeBlock>npm install @sajermann/ui-react</CodeBlock>
+				<CodeBlock>
+					<JsCode>
+						{`
+						export function CodeBlock({ children }: Props) {
+							const t = children as {
+								type: {
+									name: string;
+								};
+							};
+							console.log({ children });
+							console.log({ t });
+							// console.log(t?.type.name);
+							// console.log(t?.type.name === 'JsCode' || '');
+							return (
+								<div>
+									<code>
+										{children}
+										<OptionButton className={styles.choiseLanguage}>
+											Javascript
+										</OptionButton>
+										<OptionButton className={styles.choiseLanguage}>
+											Typescript
+										</OptionButton>
+									</code>
+								</div>
+							);
+						}
+						`}
+					</JsCode>
+				</CodeBlock>
+				<CodeBlock>
+					<JsCode>npm install @sajermann/ui-react</JsCode>
+				</CodeBlock>
+			</div>
 		</div>
 	);
 }
