310041c0bfdd30dfcd2a138d6bed40dc4621bcf9
Bruno Sajermann <sajermannbruno@gmail.com>
2022-08-23 20:12:06 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import Pages from 'vite-plugin-pages';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), Pages()],
-	base: '/',
+	base: '/NPM-SajermannUiReact/',
 	build: {
 		rollupOptions: {
 			input: {
