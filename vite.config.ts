a43bf9ff3cd46433b2d2f6a651f0101b4eff9d00
Bruno Sajermann <sajermannbruno@gmail.com>
2022-08-23 09:02:35 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), Pages()],
 	base: '/',
+	build: {
+		rollupOptions: {
+			input: {
+				main: './index.html',
+				error: './404.html',
+			},
+		},
+	},
 });
