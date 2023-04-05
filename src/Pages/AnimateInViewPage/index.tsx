b784fe0e1f4a2ac64b70c7a8d96ca0b76d619deb
Bruno Sajermann <sajermannbruno@gmail.com>
2023-04-05 18:20:28 -0300

    adjusts

--- src/Pages/AnimateInViewPage/index.tsx
+++ src/Pages/AnimateInViewPage/index.tsx
@@ -15,15 +15,21 @@ import { useInView } from 'framer-motion';
 
 function Card({ children }: { children: React.ReactNode }) {
 	const ref = useRef(null);
-	const isInView = useInView(ref, { once: false });
+	const isInView = useInView(ref, { once: true });
 
 	return (
-		<section ref={ref}>
+		<section
+			ref={ref}
+			style={{
+				height: 500,
+			}}
+		>
 			<span
 				style={{
 					transform: isInView ? 'none' : 'translateX(-200px)',
 					opacity: isInView ? 1 : 0,
 					transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+					height: 500,
 				}}
 			>
 				{children}
