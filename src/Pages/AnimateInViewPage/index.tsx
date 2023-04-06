51bc417dfd253313e286eb156a5b1740b3373ce6
Bruno Sajermann <sajermannbruno@gmail.com>
2023-04-06 07:49:39 -0300

    adjusts

--- src/Pages/AnimateInViewPage/index.tsx
+++ src/Pages/AnimateInViewPage/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { Table } from '~/Components/Table';
 import { useTranslation } from '~/Hooks/UseTranslation';
