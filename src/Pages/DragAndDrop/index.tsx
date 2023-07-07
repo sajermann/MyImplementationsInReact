de74035cb65de6dfde3f572268b56144b843f74a
Bruno Sajermann <sajermannbruno@gmail.com>
2023-07-07 14:45:42 -0300

    fix code copy

--- src/Pages/DragAndDrop/index.tsx
+++ src/Pages/DragAndDrop/index.tsx
@@ -1,15 +1,21 @@
-import { useTranslation } from '~/Hooks/UseTranslation';
-import { Main } from '~/Components/Main';
-import Section from '~/Components/Section';
-import { QuickAccessGithub } from '~/Components/QuickAccessGithub';
+import { CodeBlock } from '~/Components/CodeBlock';
 import { BrawlStar } from '~/Components/DragAndDrop/BrawlStars';
+import { Main } from '~/Components/Main';
+import { QuickAccessGithub } from '~/Components/QuickAccessGithub';
+import Section from '~/Components/Section';
+import { useTranslation } from '~/Hooks/UseTranslation';
 
 export function DragAndDropPage() {
 	const { translate } = useTranslation();
 	return (
 		<Main data-content="content-main">
 			<Section title="Drag and Drop" variant="h1">
-				{translate('IMPLEMENTS_DRAG_AND_DROP')}
+				{`${translate('IMPLEMENTS_COMPONENT')} Drag and Drop ${translate(
+					'USING_THE_LIB'
+				)} dnd-kit`}
+			</Section>
+			<Section title={translate('INSTALLATION_OF_LIB')} variant="h2">
+				<CodeBlock>npm i @dnd-kit/core;</CodeBlock>
 			</Section>
 			<Section title={translate('CODES')} variant="h2">
 				<div className="flex gap-2">
