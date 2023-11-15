b7289b03a858886ea1a8714f29020e52f114057b
Bruno Sajermann <sajermannbruno@gmail.com>
2023-11-14 21:31:43 -0300

    adjusts

--- src/Pages/Datepicker/index.tsx
+++ src/Pages/Datepicker/index.tsx
@@ -8,6 +8,8 @@ import { useState } from 'react';
 import { Button } from '~/Components/Button';
 import { CodeBlock } from '~/Components/CodeBlock';
 import { addDays, subDays } from 'date-fns';
+import { ContainerInput } from '~/Components/ContainerInput';
+import { Label } from '~/Components/Label';
 
 export function DatepickerPage() {
 	const [firstPicker, setFirstPicker] = useState(new Date().toISOString());
@@ -33,11 +35,10 @@ export function DatepickerPage() {
 
 			<Section title={translate('DATE')} variant="h2">
 				<ComponentBlock>
-					<Datepicker
-						label={translate('DATE')}
-						placeholder={translate('DD/MM/YYYY')}
-						id="Date1"
-					/>
+					<ContainerInput>
+						<Label htmlFor="Date1">{translate('DATE')}</Label>
+						<Datepicker placeholder={translate('DD/MM/YYYY')} id="Date1" />
+					</ContainerInput>
 				</ComponentBlock>
 			</Section>
 
@@ -45,21 +46,25 @@ export function DatepickerPage() {
 				<ComponentBlock>
 					<div className="flex flex-col gap-2">
 						<div className="flex gap-2">
-							<Datepicker
-								label={translate('DATE')}
-								placeholder={translate('DD/MM/YYYY')}
-								id="Date2"
-								value={firstPicker}
-								onChange={e => setFirstPicker(e.target.value)}
-							/>
+							<ContainerInput>
+								<Label htmlFor="Date2">{translate('DATE')}</Label>
+								<Datepicker
+									placeholder={translate('DD/MM/YYYY')}
+									id="Date2"
+									value={firstPicker}
+									onChange={e => setFirstPicker(e.target.value)}
+								/>
+							</ContainerInput>
 
-							<Datepicker
-								customDefaultValue={new Date()}
-								label={translate('DEFAULT_VALUE')}
-								placeholder={translate('DD/MM/YYYY')}
-								id="Date3"
-								onChange={e => setFirstPicker(e.target.value)}
-							/>
+							<ContainerInput>
+								<Label htmlFor="Date3">{translate('DEFAULT_VALUE')}</Label>
+								<Datepicker
+									customDefaultValue={new Date()}
+									placeholder={translate('DD/MM/YYYY')}
+									id="Date3"
+									onChange={e => setFirstPicker(e.target.value)}
+								/>
+							</ContainerInput>
 						</div>
 						<div className="flex items-center justify-center gap-2">
 							{firstPicker}
@@ -71,49 +76,65 @@ export function DatepickerPage() {
 
 			<Section title={translate('DATE_FORMAT')} variant="h2">
 				<ComponentBlock>
-					<Datepicker
-						dateFormat="yyyy-MM-dd"
-						label={translate('DATE')}
-						placeholder={translate('YYYY-MM-DD')}
-						id="DateFormat1"
-					/>
-
-					<Datepicker
-						dateFormat="MM/yyyy"
-						label={translate('DATE')}
-						placeholder={translate('MM/YYYY')}
-						id="DateFormat2"
-						withoutDay
-					/>
+					<ContainerInput>
+						<Label htmlFor="DateFormat1">{translate('DATE')}</Label>
+						<Datepicker
+							dateFormat="yyyy-MM-dd"
+							placeholder={translate('YYYY-MM-DD')}
+							id="DateFormat1"
+						/>
+					</ContainerInput>
+					<ContainerInput>
+						<Label htmlFor="DateFormat2">{translate('DATE')}</Label>
+						<Datepicker
+							dateFormat="MM/yyyy"
+							placeholder={translate('MM/YYYY')}
+							id="DateFormat2"
+							withoutDay
+						/>
+					</ContainerInput>
 				</ComponentBlock>
 			</Section>
 
 			<Section title={translate('DISABLED_DATES')} variant="h2">
 				<ComponentBlock>
-					<Datepicker
-						label={translate('DISABLED')}
-						placeholder={translate('DD/MM/YYYY')}
-						id="Disabled"
-						excludeDateIntervals={[
-							{ start: subDays(new Date(), 5), end: addDays(new Date(), 5) },
-						]}
-					/>
+					<ContainerInput>
+						<Label htmlFor="Disabled">{translate('DISABLED')}</Label>
+						<Datepicker
+							placeholder={translate('DD/MM/YYYY')}
+							id="Disabled"
+							excludeDateIntervals={[
+								{ start: subDays(new Date(), 5), end: addDays(new Date(), 5) },
+							]}
+						/>
+					</ContainerInput>
 				</ComponentBlock>
 			</Section>
 
 			<Section title={translate('CONTAINER_PROPS')} variant="h2">
 				<ComponentBlock>
-					<Datepicker
-						containerProps={{
-							style: {
-								width: 160,
-							},
-							className: 'flex !flex-row items-center',
+					<ContainerInput
+						style={{
+							display: 'flex',
+							flexDirection: 'row',
+							alignItems: 'center',
+							border: '1px solid',
+							width: 500,
 						}}
-						label={translate('DATE')}
-						placeholder={translate('DD/MM/YYYY')}
-						id="DateFormat3"
-					/>
+						className="p-5"
+					>
+						<Label htmlFor="DateFormat3">{translate('DATE')}</Label>
+						<Datepicker
+							containerProps={{
+								style: {
+									width: 160,
+								},
+								className: 'flex !flex-row items-center',
+							}}
+							placeholder={translate('DD/MM/YYYY')}
+							id="DateFormat3"
+						/>
+					</ContainerInput>
 				</ComponentBlock>
 			</Section>
 		</Main>
